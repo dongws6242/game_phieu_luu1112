b473c68d99cc45e8e45382af848ada2406e3e915
dongws6242 <cuk53pt2004@gmail.com>
2023-04-12 18:13:05 +0700

    Up_BackGround

--- Adventures.cpp
+++ Adventures.cpp
@@ -1,164 +1,140 @@
 #include <SDL2/SDL.h>
 #include <SDL2/SDL_image.h>
-#include <stdio.h>
+#include <iostream>
 #include <string>
 
-const int SCREEN_WIDTH = 720;
-const int SCREEN_HEIGHT = 540;
+//Screen dimension constants
+const int SCREEN_WIDTH = 900;
+const int SCREEN_HEIGHT = 600;
 
-bool init();
-bool loadMedia();
-void close();
+SDL_Window* window = NULL;
+SDL_Renderer* renderer = NULL;
+SDL_Texture* texture = NULL;
 
-SDL_Surface* loadSurface( std::string path );
-SDL_Window* gWindow = NULL;
-SDL_Surface* gScreenSurface = NULL;
-SDL_Surface* gPNGSurface = NULL;
-
+//Starts up SDL and creates window
 bool init()
 {
-	//Initialization flag
-	bool success = true;
+    //Initialize SDL
+    if(SDL_Init(SDL_INIT_VIDEO) < 0)
+    {
+        std::cout << "SDL could not initialize! SDL_Error: " << SDL_GetError() << std::endl;
+        return false;
+    }
 
-	//Initialize SDL
-	if( SDL_Init( SDL_INIT_VIDEO ) < 0 )
-	{
-		printf( "SDL could not initialize! SDL Error: %s\n", SDL_GetError() );
-		success = false;
-	}
-	else
-	{
-		//Create window
-		gWindow = SDL_CreateWindow( "Adventures of Dongws6242", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN );
-		if( gWindow == NULL )
-		{
-			printf( "Window could not be created! SDL Error: %s\n", SDL_GetError() );
-			success = false;
-		}
-		else
-		{
-			//Initialize PNG loading
-			int imgFlags = IMG_INIT_PNG;
-			if( !( IMG_Init( imgFlags ) & imgFlags ) )
-			{
-				printf( "SDL_image could not initialize! SDL_image Error: %s\n", IMG_GetError() );
-				success = false;
-			}
-			else
-			{
-				//Get window surface
-				gScreenSurface = SDL_GetWindowSurface( gWindow );
-			}
-		}
-	}
+    //Create window
+    window = SDL_CreateWindow("Adventures", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
+    if(window == NULL)
+    {
+        std::cout << "Window could not be created! SDL_Error: " << SDL_GetError() << std::endl;
+        return false;
+    }
 
-	return success;
+    //Create renderer
+    renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
+    if(renderer == NULL)
+    {
+        std::cout << "Renderer could not be created! SDL_Error: " << SDL_GetError() << std::endl;
+        return false;
+    }
+
+    //Initialize PNG loading
+    int imgFlags = IMG_INIT_PNG;
+    if(!(IMG_Init(imgFlags) & imgFlags))
+    {
+        std::cout << "SDL_image could not initialize! SDL_image Error: " << IMG_GetError() << std::endl;
+        return false;
+    }
+
+    return true;
 }
 
+//Loads media
 bool loadMedia()
 {
-	//Loading success flag
-	bool success = true;
+    //Load PNG image
+    SDL_Surface* loadedSurface = IMG_Load("C:/FisrtGame/BackGround.png");
+    if(loadedSurface == NULL)
+    {
+        std::cout << "Unable to load image! SDL Error: " << SDL_GetError() << std::endl;
+        return false;
+    }
 
-	//Load PNG surface
-	gPNGSurface = loadSurface( "C:/FisrtGame/Illustration_of_Adventures_Yugioh.png" );
-	if( gPNGSurface == NULL )
-	{
-		printf( "Failed to load PNG image!\n" );
-		success = false;
-	}
+    //Create texture from surface pixels
+    texture = SDL_CreateTextureFromSurface(renderer, loadedSurface);
+    if(texture == NULL)
+    {
+        std::cout << "Unable to create texture! SDL Error: " << SDL_GetError() << std::endl;
+        return false;
+    }
 
-	return success;
+    //Get rid of old loaded surface
+    SDL_FreeSurface(loadedSurface);
+
+    return true;
 }
 
+//Frees media and shuts down SDL
 void close()
 {
-	//Free loaded image
-	SDL_FreeSurface( gPNGSurface );
-	gPNGSurface = NULL;
+    //Free texture
+    SDL_DestroyTexture(texture);
+    texture = NULL;
 
-	//Destroy window
-	SDL_DestroyWindow( gWindow );
-	gWindow = NULL;
+    //Destroy renderer
+    SDL_DestroyRenderer(renderer);
+    renderer = NULL;
 
-	//Quit SDL subsystems
-	IMG_Quit();
-	SDL_Quit();
+    //Destroy window
+    SDL_DestroyWindow(window);
+    window = NULL;
+
+    //Quit SDL subsystems
+    IMG_Quit();
+    SDL_Quit();
 }
 
-SDL_Surface* loadSurface( std::string path )
+int main(int argc, char* argv[])
 {
-	//The final optimized image
-	SDL_Surface* optimizedSurface = NULL;
+    if(!init())
+    {
+        std::cout << "Failed to initialize!" << std::endl;
+        return -1;
+    }
 
-	//Load image at specified path
-	SDL_Surface* loadedSurface = IMG_Load( path.c_str() );
-	if( loadedSurface == NULL )
-	{
-		printf( "Unable to load image %s! SDL_image Error: %s\n", path.c_str(), IMG_GetError() );
-	}
-	else
-	{
-		//Convert surface to screen format
-		optimizedSurface = SDL_ConvertSurface( loadedSurface, gScreenSurface->format, 0 );
-		if( optimizedSurface == NULL )
-		{
-			printf( "Unable to optimize image %s! SDL Error: %s\n", path.c_str(), SDL_GetError() );
-		}
+    if(!loadMedia())
+    {
+        std::cout << "Failed to load media!" << std::endl;
+        return -1;
+    }
 
-		//Get rid of old loaded surface
-		SDL_FreeSurface( loadedSurface );
-	}
+    //Main loop flag
+    bool quit = false;
 
-	return optimizedSurface;
+    //Event handler
+    SDL_Event e;
+
+    //While application is running
+    while(!quit)
+    {
+        //Handle events on queue
+        while(SDL_PollEvent(&e) != 0)
+        {
+            //User requests quit//User requests quit
+if(e.type == SDL_QUIT)
+{
+quit = true;
 }
 
-int main( int argc, char* args[] )
-{
-	//Start up SDL and create window
-	if( !init() )
-	{
-		printf( "Failed to initialize!\n" );
-	}
-	else
-	{
-		//Load media
-		if( !loadMedia() )
-		{
-			printf( "Failed to load media!\n" );
-		}
-		else
-		{	
-			//Main loop flag
-			bool quit = false;
+//Clear screen
+SDL_RenderClear(renderer);
 
-			//Event handler
-			SDL_Event e;
+//Render texture to screen
+SDL_RenderCopy(renderer, texture, NULL, NULL);
 
-			//While application is running
-			while( !quit )
-			{
-				//Handle events on queue
-				while( SDL_PollEvent( &e ) != 0 )
-				{
-					//User requests quit
-					if( e.type == SDL_QUIT )
-					{
-						quit = true;
-					}
-				}
+//Update screen
+SDL_RenderPresent(renderer);
+}}
+close();
 
-				//Apply the PNG image
-				SDL_BlitSurface( gPNGSurface, NULL, gScreenSurface, NULL );
-			
-				//Update the surface
-				SDL_UpdateWindowSurface( gWindow );
-			}
-		}
-	}
-
-	//Free resources and close SDL
-	close();
-
-	return 0;
+return 0;
 }
